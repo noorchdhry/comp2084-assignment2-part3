cacd1f6c81ca597ad9e1de0e5e7875faa9f2ffad
Noor Chaudhry <noor.m.chaudhry@gmail.com>
2019-04-08 11:32:35 -0400

    Updated Code
    Upload all updates till this point

---
+++ a2p3/a2p3/Models/DbModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace a2p3.Models
+{
+    public class DbModel : DbContext 
+    {
+        public DbModel(DbContextOptions<DbModel>options) : base(options)
+        {
+
+        }
+
+        public DbSet<TaskList> TaskLists { get; set; }
+    }
+}
---
+++ a2p3/a2p3/Models/TaskList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace a2p3.Models
+{
+    public class TaskList
+    {
+        [Key]
+        public int TaskID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TaskName { get; set; }
+
+        public bool TaskStatus { get; set; }
+
+        [StringLength(100)]
+        public string Description { get; set; }
+
+    }
+}
